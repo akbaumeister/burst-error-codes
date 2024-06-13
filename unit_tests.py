33d08187250d795b312c0265ae1144b426239e03
akbaumeister <akbaumeister@gmail.com>
2024-06-13 10:56:33 +0200

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -1,10 +1,7 @@
 import scipy.special
-
+from utils import *
 from lattice_algorithms import *
 import matplotlib.pyplot as plt
-
-n = 2
-q = 5
 
 
 # c = [2,0,2,0]
@@ -30,27 +27,24 @@ q = 5
 
 # test uniform sampling
 t = 3
-n = 5
+n = 4
 
 keys = [str(e) for e in enumerate_sphere(n, t)]
 vecs = dict.fromkeys(keys, 0)
 
-for i in range(10000000):
+for i in range(100000):
     v = sample_uniformly_An(n, t)
     vecs[str(v)] += 1
 
 plt.bar(vecs.keys(), vecs.values())
 
 #for export
-plt.axis('off')
-plt.savefig('sampling_nonuniform_n5_t3.png', bbox_inches='tight', pad_inches=0, dpi=1200)
+# plt.axis('off')
+# plt.savefig('sampling_nonuniform_n5_t3.png', bbox_inches='tight', pad_inches=0, dpi=1200)
 
 y_pos = range(len(keys))
 plt.xticks(y_pos, vecs.keys(), rotation=90)
 
-# test enumerate
-# print(list(enumerate_sphere(3, 3)))
-# print(len(list(enumerate_sphere(3, 3))))
-
-print(*(k for k, v in vecs.items() if v==0))
 plt.show()
+
+# print(random_partition_weighted(3,3,20))
